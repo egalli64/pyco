db048b6a15206ef41eee14a3b67e69a4733edf1f
Emanuele <egalli64@gmail.com>
2023-11-25 20:30:18 +0100

    variables

---
+++ m1/s2_variables.py
@@ -0,0 +1,21 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 1 - Fundamental concepts
+
+Variables
+"""
+
+# defining a string variable
+message = "Hello!"
+
+# using a previously defined variable
+print(message)
+
+# using an undefined variable is a (bad) mistake
+# print(mesage)
+
+# the type of the variable stored in message
+print(type(message))
