f4aa1c652e492fa694f6ffdf59b3810bc4142964
Emanuele <egalli64@gmail.com>
2023-11-26 18:00:34 +0100

    concatenation and repetition

---
+++ m1/s7_string_concat.py
@@ -0,0 +1,33 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 1 - Fundamental concepts
+
+String concatenation and repetition
+"""
+
+message = "Resistence" + " is " + "useless"
+print(message)
+
+try:
+    message = "it is not " + 2 + " late"
+except TypeError:
+    print("Concatenation works only on strings!")
+
+# same as: message = message + "!"
+message += "!"
+print(message)
+
+message_2 = "hello " * 3
+print(message_2)
+
+try:
+    message_2 = 3.4 * "hello "
+except TypeError:
+    print("Repetition works only on one string and one integer!")
+
+# same as: message_2 = message_2 * 3
+message_2 *= 2
+print(message_2)
