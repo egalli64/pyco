02b5a4bbf4198581e6b357b9dec8577441158b58
Emanuele <egalli64@gmail.com>
2023-11-26 17:17:06 +0100

    precedence

---
+++ m1/s6_precedence.py
@@ -0,0 +1,21 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 1 - Fundamental concepts
+
+Operator precedence and association
+"""
+
+# by default, multiply first
+print(2 + 6 * 7 - 3)      # 41
+
+# rearrange in the preferred way
+print((2 + 6) * (7 - 3))  # 32
+
+# TODO: use parentheses to fix the bug
+x = 1.0
+result = 1 / x + 1 / x + 1 / x + 1 / x
+print("Actual result is:", result)
+print("Expected result is: 0.6000000000000001")
