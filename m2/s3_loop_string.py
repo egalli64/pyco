4f6ae1bc26247883ae70b509d50e3ff8a2e8f4e0
Emanuele <egalli64@gmail.com>
2023-11-27 12:15:56 +0100

    loop on a string

---
+++ m2/s3_loop_string.py
@@ -0,0 +1,34 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 2 - Control Flow
+
+While and for loop on a string
+"""
+s = "Python"
+
+print("while loop")
+
+# while index is in string
+print("[0 .. len):", end="\t")
+i = 0
+while i < len(s):
+    print(s[i], end=" ")
+    i += 1
+print()
+
+print("\nfor loops")
+
+# for each index in string
+print("[0 .. len):", end="\t")
+for i in range(len(s)):
+    print(s[i], end=" ")
+print()
+
+# for each char in string
+print("[P .. n]:", end="\t")
+for c in s:
+    print(c, end=" ")
+print()
