747d51a12a76fffc742392552042e9d10b8805d7
Emanuele <egalli64@gmail.com>
2023-11-27 22:09:21 +0100

    dictionary definition

---
+++ m4/s2_definition.py
@@ -0,0 +1,20 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 3 - Dictionary
+
+Definition
+"""
+# a dictionary representing a user
+alice = {'name': 'Alice', 'age': 32, 'city': 'Madrid'}
+print(alice)
+
+# another dictionary for another user
+bob = dict(name='Bob', age=42, city='Berlin')
+print(bob)
+
+# a dictionary representing more users
+users = {1: alice, 2: bob}
+print(users)
