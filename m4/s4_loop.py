2f992c8cb9dfc2a9a9f8b96b281bb724b77535dd
Emanuele <egalli64@gmail.com>
2023-11-28 20:05:39 +0100

    looping

---
+++ m4/s4_loop.py
@@ -0,0 +1,60 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 4 - Dictionary
+
+"""
+friends = {"kim": 1284, "bob": 4423, "tom": 1284, "jim": 1299}
+
+print("The dictionary:", friends)
+print("As list of tuples:", friends.items())
+print("The keys:", friends.keys())
+print("The values:", friends.values())
+print("---")
+
+print("All the pairs:")
+for key, value in friends.items():
+    print(f"{key}, {value}")
+print("---")
+
+print("All the keys:")
+for key in friends.keys():
+    print(key)
+print("---")
+
+print("All the keys (implicit):")
+for key in friends:
+    print(key)
+print("---")
+
+print("All the values:")
+for value in friends.values():
+    print(value)
+print("---")
+
+print("All the keys (sorted):")
+for key in sorted(friends.keys()):
+    print(key)
+print("---")
+
+print("All the values (sorted):")
+for value in sorted(friends.values()):
+    print(value)
+print("---")
+
+print("All the pairs (sorted):")
+for key, value in sorted(friends.items()):
+    print(f"{key}, {value}")
+print("---")
+
+print("All the values (no duplicates):")
+for value in set(friends.values()):
+    print(value)
+print("---")
+
+print("All the values (no duplicates, sorted):")
+for value in sorted(set(friends.values())):
+    print(value)
+print("---")
