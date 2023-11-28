afb1edd0400d6c8b88a6a5d2ed45d8b40d6505b1
Emanuele <egalli64@gmail.com>
2023-11-28 17:26:21 +0100

    access by key

--- m4/s2_definition.py
+++ m4/s2_definition.py
@@ -8,13 +8,21 @@ Module 3 - Dictionary
 Definition
 """
 # a dictionary representing a user
-alice = {'name': 'Alice', 'age': 32, 'city': 'Madrid'}
+alice = {"name": "Alice", "age": 32, "city": "Madrid"}
 print(alice)
+print("Alice age is", alice["age"])
 
 # another dictionary for another user
-bob = dict(name='Bob', age=42, city='Berlin')
+bob = dict(name="Bob", age=42, city="Berlin")
 print(bob)
+print("Bob city is", bob["city"])
 
 # a dictionary representing more users
 users = {1: alice, 2: bob}
 print(users)
+print("User 1 is", users[1])
+
+try:
+    print(users[3])
+except KeyError:
+    print("Can't access a value for a missing key!")
