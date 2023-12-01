725d55be19f90a0b37c8623ac4c76452584390b3
Emanuele <egalli64@gmail.com>
2023-12-01 17:01:33 +0100

    iterator

---
+++ m5/s6_iterator.py
@@ -0,0 +1,46 @@
+"""
+Python Course - Base
+
+https://github.com/egalli64/pycoba
+
+Module 5 - Function
+
+Iterator
+"""
+values = [1, 2, 3, 4, 5]
+
+# get an iterator from the iterable
+it = iter(values)
+
+# use the iterator to loop on the elements
+print("Looping by explicit iterator: ", end="")
+for cur in it:
+    print(cur, end=" ")
+print()
+
+# the iterator is exhausted, reset it
+it = iter(values)
+print("The first value is", next(it))
+
+# create another iterator on the same iterable
+it_2 = iter(values)
+print("it_2 gives", next(it_2))
+print("it gives", next(it))
+
+# complete the loop on an iterator
+while True:
+    try:
+        value = next(it)
+        print("and then", value)
+    except StopIteration:
+        print("No more values in the current iteration")
+        break
+
+# the other iterator is still usable
+print("it_2 gives", next(it_2))
+
+# usually, let Python do the dirty job
+print("Looping by implicit iterator: ", end="")
+for value in values:
+    print(value, end=" ")
+print()
