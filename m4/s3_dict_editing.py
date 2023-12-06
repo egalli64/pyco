150a11915b8e8aa96f12ca0e2f67229f005a81fd
Emanuele <egalli64@gmail.com>
2023-12-06 22:42:13 +0100

    pop

--- m4/s3_dict_editing.py
+++ m4/s3_dict_editing.py
@@ -10,32 +10,40 @@ Dictionary: insert, edit, remove, check
 friends = {}
 print("An empty dictionary:", friends)
 
-# add two pairs
+# add three items
 friends["bob"] = 4423
 friends["jim"] = 9812
-print("After adding bob and jim:", friends)
+friends["tom"] = 3412
+print("After adding bob, jim, and tom:", friends)
 
 # edit a value
 friends["jim"] = 1298
 print("After changing jim value:", friends)
 
 # change and assign a value
-friends["jim"] += 1
-print("After increasing jim value:", friends)
-
 try:
-    friends["tom"] += 1
+    friends["jim"] += 1
+    print("After increasing jim value:", friends)
+    friends["joe"] += 1
 except KeyError:
     print("Can't change a value for a missing key!")
 
-# remove a pair
-del friends["jim"]
-print("After deleting jim:", friends)
+# remove items
+try:
+    del friends["jim"]
+    print("After deleting jim:", friends)
+
+    del friends["jim"]
+except KeyError:
+    print("Can't delete an item for a missing key!")
 
 try:
-    del friends["tom"]
+    value = friends.pop("tom")
+    print("tom's value was", value)
+
+    friends.pop("tom")
 except KeyError:
-    print("Can't delete a pair for a missing key!")
+    print("Can't pop an item for a missing key!")
 
 # check for presence
 if "bob" in friends:
