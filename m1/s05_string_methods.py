c01178ba77b4c3e736c61eaa905f7488644f2842
Emanuele <egalli64@gmail.com>
2023-12-07 15:59:43 +0100

    len

--- m1/s05_string_methods.py
+++ m1/s05_string_methods.py
@@ -9,16 +9,16 @@ A few string methods
 """
 
 s = "welcome to Python"
-print("A string:", s)
+print(f"The length of '{s}' is", len(s))
 
 # Uppercase / Lowercase
 print("Capitalized:", s.capitalize())
 print("Titled:", s.title())
-print("Uppercase:", s.upper())
-print("Lowercase:", s.lower())
+print("Uppercased:", s.upper())
+print("Lowercased:", s.lower())
 
 # Strip
-t = "  welcome to Python  "
+t = "  welcome to Python!  "
 print(f"A string: '{t}'")
 print(f"Left strip: '{t.lstrip()}'")
 print(f"Right strip: '{t.rstrip()}'")
@@ -31,15 +31,17 @@ print(f"'{s}' ends with '{p}'?", s.endswith(p))
 
 # find a substring
 j = "Java"
-print(f"In which position '{j}' is in '{s}'?", s.find(j))
-print(f"In which position '{p}' is in '{s}'?", s.find(p))
+print(f"Find '{j}' in '{s}':", s.find(j))
+print(f"Find '{p}' in '{s}':", s.find(p))
 
-print(f"In which position '{p}' is in '{s}'?", s.index(p))
+# index of the first substring
 try:
-    print(f"In which position '{j}' is in '{s}'?", end=" ")
+    print(f"Index of '{p}' in '{s}':", s.index(p))
+    print(f"Index of '{j}' in '{s}':", end=" ")
     print(s.index(j))
 except ValueError:
     print("Not found!")
 
+# count substrings
 print(f"Count occurrences of '{j}' in '{s}':", s.count(j))
 print(f"Count occurrences of '{p}' in '{s}':", s.count(p))
