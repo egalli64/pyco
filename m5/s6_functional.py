e20ed05fb52d5f1a74a5e9501c1aa78f8ad385bf
Emanuele <egalli64@gmail.com>
2023-12-09 11:52:14 +0100

    functional programming

---
+++ m5/s6_functional.py
@@ -0,0 +1,47 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 5 - Function
+
+Functional programming
+"""
+
+
+def greeter():
+    """Definition of a simple function"""
+    print("Hello")
+
+
+# A function is an object
+print("The type of greeter is", type(greeter))
+
+# invoking a simple function
+greeter()
+
+
+def runner(f):
+    """Definition of a simple Higher-Order Function (HOF)"""
+    print("Running the passed function:", end=" ")
+    f()
+
+
+# invoking a HOF
+runner(greeter)
+
+
+def runner2(f, left, right):
+    """HOF for a function accepting two arguments"""
+    print(f"Running the passed function on {left} and {right}:", end=" ")
+    f(left, right)
+
+
+def adder(left, right):
+    print(left + right)
+
+
+# invoking the adder function
+adder(24, 18)
+# passing the adder function to a HOF
+runner2(adder, 24, 18)
