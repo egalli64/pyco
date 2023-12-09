0a5d16587f1838edfb0259dbb78a62e55852f59e
Emanuele <egalli64@gmail.com>
2023-12-09 10:19:54 +0100

    moved

---
+++ m8/s3_write.py
@@ -0,0 +1,25 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 8 - File
+
+Write
+"""
+FILENAME = "my_file.txt"
+friends = ["Tom", "Jenny", "Bob", "Kim", "Micky", "Lee"]
+
+# (re)write the file
+with open(FILENAME, "w") as f:
+    # print each friend in the file
+    for friend in friends:
+        print(friend, end=" ", file=f)
+    print(file=f)
+
+# same, but append, and using write instead of print
+with open(FILENAME, "a") as f:
+    for friend in friends:
+        f.write(friend)
+        f.write(" ")
+    f.write("\n")
