cb5b0c7aa7b93ced5bc08676de21f8475a274046
Emanuele <egalli64@gmail.com>
2023-12-10 22:30:17 +0100

    isinstance

--- m1/s03_variables.py
+++ m1/s03_variables.py
@@ -15,19 +15,25 @@ unknown = None
 ANSWER = 42
 
 # using previously defined variables
-print(message)
-print(ANSWER)
-print(unknown)
+print("message:", message)
+print("unknown", unknown)
+print("ANSWER:", ANSWER)
 
-# using an undefined variable is a (Name) error
+# using an undefined variable is a NameError
 # print(mesage)
 
 # the type of a variable is the type of the object it currently references
-print(type(message))
-print(type(ANSWER))
-print(type(unknown))
+print("Type of message is", type(message))
+print("Type of unknown is", type(unknown))
+print("Type of ANSWER is", type(ANSWER))
+
+# check if the type is the expected one by isinstance
+print("Is message a str?", isinstance(message, str))
+print("Is message an int?", isinstance(message, int))
+print("Is ANSWER a float or an int?", isinstance(ANSWER, (float, int)))
+print("Is ANSWER a str?", isinstance(ANSWER, str))
 
 # the unique id associated with the referenced objects
-print(id(message))
-print(id(ANSWER))
-print(id(unknown))
+print("message id:", id(message))
+print("unknown id:", id(unknown))
+print("ANSWER id:", id(ANSWER))
