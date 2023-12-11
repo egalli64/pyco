f51f3b8b1dbdbfc0437289d0aec969e09727b18c
Emanuele <egalli64@gmail.com>
2023-12-11 17:44:28 +0100

    object

---
+++ m10/s2_object.py
@@ -0,0 +1,23 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 10 - More on Object Oriented Programming 
+
+The object class
+"""
+
+
+class A(object):
+    pass
+
+
+print("The A base class is", A.__base__)
+
+
+class B:
+    pass
+
+
+print("The B base class is", B.__base__)
