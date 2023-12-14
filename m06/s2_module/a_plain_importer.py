d099bf074898a7022df50fc8456cee3d2586a4d5
Emanuele <egalli64@gmail.com>
2023-12-14 10:04:50 +0100

    import

---
+++ m06/s2_module/a_plain_importer.py
@@ -0,0 +1,16 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 6 - Modules and Packages
+
+Plain import of a module
+"""
+import a_module
+
+# calling a function defined in the imported module
+a_module.greeting("Tom")
+
+# accessing a variable (constant) defined in the imported module
+print("The application name is", a_module.APPLICATION_NAME)
