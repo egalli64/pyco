2c5d0bf17cb17eeccd1c6ca5c36723ceabade6d9
Emanuele <egalli64@gmail.com>
2023-12-14 10:10:16 +0100

    main guard added

--- m06/s2_module/a_module.py
+++ m06/s2_module/a_module.py
@@ -13,3 +13,8 @@ APPLICATION_NAME = "Skynet"
 def greeting(name):
     """A simple function"""
     print(f"Hello, {name}! I'm {APPLICATION_NAME}")
+
+
+if __name__ == "__main__":
+    print("This file shouldn't be run as a script")
+    greeting("Unknown")
