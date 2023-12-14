0507b06cd3fd3dd5c309eb0e0923a7c11d084da2
Emanuele <egalli64@gmail.com>
2023-12-14 10:41:10 +0100

    from import as

---
+++ m06/s2_module/with_alias_from.py
@@ -0,0 +1,16 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 6 - Modules and Packages
+
+Import a module using alias
+"""
+from a_module import greeting as am_greeter, APPLICATION_NAME as AM_NAME
+
+# calling an aliased function defined in the imported module
+am_greeter("Tom")
+
+# accessing an aliased constant defined in the imported module
+print("The application name is", AM_NAME)
