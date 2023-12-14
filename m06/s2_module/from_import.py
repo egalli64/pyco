16bc27e945a78e254e19a379ebe1fad60de2fd29
Emanuele <egalli64@gmail.com>
2023-12-14 10:28:43 +0100

    from import

---
+++ m06/s2_module/from_import.py
@@ -0,0 +1,16 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 6 - Modules and Packages
+
+Filtered import from a module
+"""
+from a_module import greeting
+
+# calling a function defined in the imported module
+greeting("Tom")
+
+# uncomment next line to get a NameError
+# print("The application name is", APPLICATION_NAME)
