6904afbf86c232702cdbf37b1afd60cd8f205922
Emanuele <egalli64@gmail.com>
2023-12-14 13:00:32 +0100

    module search path

---
+++ m06/s3_search/csv.py
@@ -0,0 +1,10 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 6 - Modules and Packages
+
+Module search path
+"""
+print("Not a good idea creating a module with the same name of a standard one!")
---
+++ m06/s3_search/main.py
@@ -0,0 +1,30 @@
+"""
+Python Course
+
+https://github.com/egalli64/pyco
+
+Module 6 - Modules and Packages
+
+Module search path
+"""
+import sys
+import csv
+
+try:
+    # this module is not expected to exist
+    import a_missing_one
+except ModuleNotFoundError as ex:
+    print(ex)
+
+# highest priority modules
+print("The tuple of built-in modules:", sys.builtin_module_names)
+
+# paths for the (other) modules that could be imported
+print("The current module search path:")
+for current in sys.path:
+    print(current)
+
+try:
+    print(csv.__version__)
+except AttributeError as ex:
+    print("What's going on? -->", ex)
