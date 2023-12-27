ff217592fa631da1a15d6a9dab215bd4f68f2ecc
Emanuele <egalli64@gmail.com>
2023-12-27 19:52:21 +0100

    zip and list

---
+++ p2/m1_basic_types/s5_list_zip.py
@@ -0,0 +1,16 @@
+"""
+Python Course - Part 2
+
+https://github.com/egalli64/pyco
+
+Module 1 - More on basic types
+
+List - zip
+"""
+names = ["tom", "bob", "kim", "jim"]
+colors = ["green", "blue", "yellow", "red"]
+
+couples = list(zip(names, colors))
+
+for name, color in couples:
+    print(f"{color} is {name}'s preferred color")
