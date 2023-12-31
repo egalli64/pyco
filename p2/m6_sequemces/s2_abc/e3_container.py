d284e234aa33d64376cc61c1c6560b5b5ed4bc7a
Emanuele <egalli64@gmail.com>
2023-12-31 12:58:36 +0100

    basic ABC

---
+++ p2/m6_sequemces/s2_abc/e3_container.py
@@ -0,0 +1,23 @@
+"""
+Python Course - Part 2
+
+https://github.com/egalli64/pyco
+
+Module 6 - Sequence
+
+ABC - Abstract Base Class - collections abc Container
+"""
+from collections import abc
+
+
+class MyContainer(abc.Container):
+    """A concrete Container"""
+
+    def __contains__(self, x):
+        """All the odd values are in this container!"""
+        return x % 2
+
+
+xs = MyContainer()
+for x in range(-2, 3):
+    print(f"Is {x} in my container?", x in xs)
