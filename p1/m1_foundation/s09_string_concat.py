9144be474d46b65c71b07769c53ff9696f21bc3b
Emanuele <egalli64@gmail.com>
2024-01-03 10:29:09 +0100

    clarify

--- p1/m1_foundation/s09_string_concat.py
+++ p1/m1_foundation/s09_string_concat.py
@@ -7,11 +7,23 @@ Module 1 - Fundamental concepts
 
 String concatenation and repetition
 """
+a = "alpha"
+b = "beta"
 
+# assign to s the concatenation of a and b
+s = a + b
+print(s)
+
+# assign to s the concatenation of s and b
+s += b
+print(s)
+
+# assign to message the concatenation of first, second, and third string to right
 message = "Resistence" + " is " + "useless"
 print(message)
 
 try:
+    # can't concatenate string and non-string
     message = "it is not " + 2 + " late"
 except TypeError:
     print("Concatenation works only on strings!")
@@ -20,10 +32,12 @@ except TypeError:
 message += "!"
 print(message)
 
+# assign to rhs the "multiplication" of the rhs string by 3
 message_2 = "hello " * 3
 print(message_2)
 
 try:
+    # can't multiply string and non-int
     message_2 = 3.4 * "hello "
 except TypeError:
     print("Repetition works only on one string and one integer!")
