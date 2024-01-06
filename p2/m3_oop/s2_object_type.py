bf46efab422c5408bbed0a85ec3ba0846155ccab
Emanuele <egalli64@gmail.com>
2024-01-06 11:14:09 +0100

    more info

--- p2/m3_oop/s2_object_type.py
+++ p2/m3_oop/s2_object_type.py
@@ -5,7 +5,7 @@ https://github.com/egalli64/pyco
 
 Module 3 - More on Object Oriented Programming
 
-The class object and type
+The classes object and type
 """
 
 
@@ -13,19 +13,20 @@ class A(object):
     pass
 
 
-print("The A base class is", A.__base__)
-
-
 class B:
     pass
 
 
-print("The B base class is", B.__base__)
+print(f"The {A.__name__} base class is {A.__base__}")
+print(f"The {B.__name__} base class is {B.__base__}")
 
 print("\n*** The object attributes:")
 for cur in dir(object):
     print(cur)
+print()
 
-print("\n*** The type attributes:")
+# the metaclass type is-a object
+print(f"{type.__name__}, that extends {type.__base__}, is callable?", callable(type))
+print("*** The type attributes:")
 for cur in dir(type):
     print(cur)
