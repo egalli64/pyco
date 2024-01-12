d94f057a7639d46312ac308e00813f34d5aac5c5
Emanuele <egalli64@gmail.com>
2024-01-12 14:35:41 +0100

    bool operators

---
+++ p3/m1_types/s3b_bool_operators.py
@@ -0,0 +1,53 @@
+"""
+Python Course - Part 3
+
+https://github.com/egalli64/pyco
+
+Module 1 - Types
+
+bool operators
+"""
+a = True
+b = False
+
+print("AND truth table")
+for a in False, True:
+    for b in False, True:
+        print(f"{a} and {b} is", a and b)
+print()
+
+print("OR truth table")
+for a in False, True:
+    for b in False, True:
+        print(f"{a} or {b} is", a or b)
+print()
+
+print("NOT truth table")
+for a in False, True:
+    print(f"not {a} is", not a)
+print()
+
+print("XOR truth table")
+for a in False, True:
+    for b in False, True:
+        print(f"{a} != {b} is", a != b)
+print()
+
+print("Using bitwise operator (meh)")
+print("AND truth table")
+for a in False, True:
+    for b in False, True:
+        print(f"{a} & {b} is", a & b)
+print()
+
+print("OR truth table")
+for a in False, True:
+    for b in False, True:
+        print(f"{a} | {b} is", a | b)
+print()
+
+print("XOR truth table")
+for a in False, True:
+    for b in False, True:
+        print(f"{a} ^ {b} is", a ^ b)
+print()
