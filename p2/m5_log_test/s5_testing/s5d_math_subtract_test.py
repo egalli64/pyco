66d56bb67e42c3f38d286da7b8c63f72c9ca8c59
Emanuele <egalli64@gmail.com>
2024-01-18 10:30:05 +0100

    assertAlmostEqual delta

--- p2/m5_log_test/s5_testing/s5d_math_subtract_test.py
+++ p2/m5_log_test/s5_testing/s5d_math_subtract_test.py
@@ -21,9 +21,13 @@ class TestMathSubtract(unittest.TestCase):
         """3.0 - 2.1 is _not_ equal to 0.9!"""
         self.assertNotEqual(m.subtract(3.0, 2.1), 0.9)
 
-    def test_subtract_close_enough(self):
-        """3.0 - 2.1 is very close to 0.9"""
+    def test_subtract_close_places(self):
+        """3.0 - 2.1 is very close to 0.9, but increase the number of decimal places and the test fails"""
         self.assertAlmostEqual(m.subtract(3.0, 2.1), 0.9, places=15)
+
+    def test_subtract_close_delta(self):
+        """3.0 - 2.1 is very close to 0.9, but decrease the delta and the test fails"""
+        self.assertAlmostEqual(m.subtract(3.0, 2.1), 0.9, delta=2e-16)
 
 
 if __name__ == "__main__":
