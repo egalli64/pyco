4be8c408dda1c8872088b73472969a17c030b3c8
Emanuele <egalli64@gmail.com>
2024-01-19 17:48:55 +0100

    minor review

--- p1/m1_foundation/s08_precedence/e2_exercise.py
+++ p1/m1_foundation/s08_precedence/e2_exercise.py
@@ -7,13 +7,6 @@ Module 1 - Fundamental concepts
 
 Operator precedence and association
 """
-
-# by default, multiply first
-print(2 + 6 * 7 - 3)      # 41
-
-# rearrange in the preferred way
-print((2 + 6) * (7 - 3))  # 32
-
 # TODO: use parentheses to fix the bug
 x = 1.0
 result = 1 / x + 1 / x + 1 / x + 1 / x
