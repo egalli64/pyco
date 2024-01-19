e683a51426f31cd31e10e22c034343fa85eddd07
Emanuele <egalli64@gmail.com>
2024-01-19 13:11:37 +0100

    minor review

--- p1/m1_foundation/s02_input_output/e1_print.py
+++ p1/m1_foundation/s02_input_output/e1_print.py
@@ -6,6 +6,7 @@ https://github.com/egalli64/pyco
 Module 1 - Fundamental concepts
 
 Input and output
+Print to standard output
 """
 # print a single string to the console
 print("Hello, Python")
@@ -22,13 +23,3 @@ print("Python", end="")
 
 # print just a newline
 print()
-
-name = input("What's your name? ")
-print("Hello,", name)
-
-# get the user input, providing a prompt
-s = input("How old are you? ")
-
-# convert the string to int (beware of ValueError)
-age = int(s)
-print("I wonder if you are really ", age, ", ", name, sep="")
