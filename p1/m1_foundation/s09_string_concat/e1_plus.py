2a64d50c53d038f8703a33bbb0cc86408b87085f
Emanuele <egalli64@gmail.com>
2024-01-19 18:22:33 +0100

    minor review

--- p1/m1_foundation/s09_string_concat/e1_plus.py
+++ p1/m1_foundation/s09_string_concat/e1_plus.py
@@ -5,43 +5,31 @@ https://github.com/egalli64/pyco
 
 Module 1 - Fundamental concepts
 
-String concatenation and repetition
+String concatenation
 """
 a = "alpha"
 b = "beta"
+print(f"a is {a}, b is {b}\n")
 
 # assign to s the concatenation of a and b
 s = a + b
-print(s)
+print(f"{a} + {b} =", s)
 
 # assign to s the concatenation of s and b
+print(f"{s} += {b} =", end=" ")
 s += b
-print(s)
+print(s, "\n")
 
 # assign to message the concatenation of first, second, and third string to right
 message = "Resistence" + " is " + "useless"
-print(message)
+print(message, "\n")
 
 try:
     # can't concatenate string and non-string
     message = "it is not " + 2 + " late"
 except TypeError:
-    print("Concatenation works only on strings!")
+    print("Concatenation works only on strings!\n")
 
 # same as: message = message + "!"
 message += "!"
 print(message)
-
-# assign to rhs the "multiplication" of the rhs string by 3
-message_2 = "hello " * 3
-print(message_2)
-
-try:
-    # can't multiply string and non-int
-    message_2 = 3.4 * "hello "
-except TypeError:
-    print("Repetition works only on one string and one integer!")
-
-# same as: message_2 = message_2 * 3
-message_2 *= 2
-print(message_2)
---
+++ p1/m1_foundation/s09_string_concat/e2_star.py
@@ -0,0 +1,22 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Fundamental concepts
+
+String repetition
+"""
+# assign to message the "multiplication" of the hello string by 3
+message = "hello " * 3
+print(message)
+
+try:
+    # can't multiply string and non-int
+    message = 3.4 * "hello "
+except TypeError:
+    print("Repetition works only on one string and one integer!")
+
+# same as: message = message * 2
+message *= 2
+print(message)
