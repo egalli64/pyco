dc770e2a5e408e242d33f21aca6c14d4cd315d6b
Emanuele <egalli64@gmail.com>
2024-01-20 17:50:37 +0100

    print & input

---
+++ p1/m1_string/s02_input_output/e1_print.py
@@ -0,0 +1,25 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Strings and variables
+
+Input and output
+Print to standard output
+"""
+# print a single string to the console
+print("Hello, Python")
+
+# print more arguments
+print("Hello", "Python")
+
+# use an underscore as separator
+print("Hello", "Python", sep="_")
+
+# do not add a newline at the end of each print
+print("Hello", end="")
+print("Python", end="")
+
+# print just a newline
+print()
