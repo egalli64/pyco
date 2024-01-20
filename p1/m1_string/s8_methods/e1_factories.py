629a79b56b0413ebaadc7c1a6bc984097db29246
Emanuele <egalli64@gmail.com>
2024-01-20 22:26:31 +0100

    a few string methods

---
+++ p1/m1_string/s8_methods/e1_factories.py
@@ -0,0 +1,30 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Strings and variables
+
+String modifier factories
+"""
+# A string
+s = "welcome to Python's world"
+
+# Uppercase / Lowercase
+print("Capitalized:", s.capitalize())
+print("Titled:", s.title())
+print("Uppercased:", s.upper())
+print("Lowercased:", s.lower())
+print()
+
+# Strip
+t = "  welcome to python!  "
+print(f"A string: '{t}'")
+print(f"Left strip: '{t.lstrip()}'")
+print(f"Right strip: '{t.rstrip()}'")
+print(f"Left/right strip: '{t.strip()}'")
+print()
+
+# Replace
+print("Replacing Python:", s.replace("Python", "Programming"))
+print("Replacing 'o's:", s.replace("o", "_"))
---
+++ p1/m1_string/s8_methods/e2_search.py
@@ -0,0 +1,26 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Strings and variables
+
+Search in string
+"""
+# The target string
+s = "welcome to Python"
+
+# boolean check on start/end
+p = "Python"
+print(f"'{s}' starts with '{p}'?", s.startswith(p))
+print(f"'{s}' ends with '{p}'?", s.endswith(p))
+
+# find: index of a substring (or -1)
+j = "Java"
+print(f"Find '{j}' in '{s}':", s.find(j))
+print(f"Find '{p}' in '{s}':", s.find(p))
+
+# count substrings
+print(f"Count occurrences of '{j}' in '{s}':", s.count(j))
+print(f"Count occurrences of '{p}' in '{s}':", s.count(p))
+print(f"Count occurrences of 'o' in '{s}':", s.count("o"))
