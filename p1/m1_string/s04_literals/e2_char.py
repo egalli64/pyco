486273d7bc523ef63e397eaffd19d12a87e92bda
Emanuele <egalli64@gmail.com>
2024-01-20 18:08:10 +0100

    string literals

---
+++ p1/m1_string/s04_literals/e2_char.py
@@ -0,0 +1,26 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Strings and variables
+
+String literals
+Single char
+"""
+# a Python char is a string sized 1
+s4 = "x"
+print(type(s4), s4)
+
+# get the Unicode code point representation of a char
+code_x = ord(s4)
+print(type(code_x), code_x)
+
+# the integer representation for the Unicode code point of a grinning face
+code_grin = 0x1F600
+s5 = chr(code_grin)
+print("A Unicode code and its associated character:", hex(code_grin), s5)
+
+# escape sequences
+s6 = "a multiline string\nis 'useful' to represent \"long\" text\nthat should be read by humans"
+print(s6)
---
+++ p1/m1_string/s04_literals/e3_fstring.py
@@ -0,0 +1,15 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Strings and variables
+
+String literals
+F-string
+"""
+PI = 3.141592653589793
+radius = 5
+
+print(f"The area of a circle with radius {radius} is {PI * radius ** 2}")
+print(f"A pi approximation: {PI:.5f}")
