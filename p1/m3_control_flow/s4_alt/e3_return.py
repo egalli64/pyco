d3034b444b9969c0640a0e1b6fce2fc681c6ac8f
Emanuele <egalli64@gmail.com>
2024-01-22 16:01:31 +0100

    return to terminate a loop

---
+++ p1/m3_control_flow/s4_alt/e3_return.py
@@ -0,0 +1,30 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 3 - Control Flow
+
+Alterate / terminate a loop
+The return statement
+"""
+s = "Welcome To Pythonville"
+print(f"Checking string '{s}'\n")
+
+
+def print_first_word(s):
+    """
+    A function that prints up to the first blank
+    """
+    for c in s:
+        if c == " ":
+            print()
+            return
+        else:
+            print(c, end="")
+    print()
+
+
+print("Return as soon as a blank is found:")
+print_first_word(s)
+print_first_word("Hello")
