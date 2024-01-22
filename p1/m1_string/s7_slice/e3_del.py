8e2146c5123810dfbe34fa412ee10385a09a940f
Emanuele <egalli64@gmail.com>
2024-01-22 18:24:39 +0100

    slice del

---
+++ p1/m1_string/s7_slice/e3_del.py
@@ -0,0 +1,30 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Strings and variables
+
+Sequences - del on slice
+"""
+# a string is a sequence
+str = "welcome to Python's world"
+print("A string:", str)
+# a list is a sequence, too
+lis = ["bob", "tom", "kim", "tim"]
+print("A list:", lis)
+print()
+
+# strings are immutable sequences
+try:
+    del str[1:3]
+except TypeError as ex:
+    print(ex)
+
+# lists are mutable sequences
+del lis[1:3]
+print("After slice del on the mutable sequence:", lis)
+
+# wrong indices mean do not delete anything
+del lis[42:25]
+print("After wrong slice del on the mutable sequence (nothing to delete):", lis)
