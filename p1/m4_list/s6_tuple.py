380725d26ada9502f83eb8bf92f7df62b2236d8f
Emanuele <egalli64@gmail.com>
2024-01-22 22:35:33 +0100

    review

--- p1/m4_list/s6_tuple.py
+++ p1/m4_list/s6_tuple.py
@@ -3,13 +3,13 @@ Python Course - Part 1
 
 https://github.com/egalli64/pyco
 
-Module 3 - List
+Module 4 - List
 
 Tuple
 """
 # a tuple
 friends = ("bob", "tom", "kim", "abe", "luc")
-print("friends:", friends)
+print(f"friends in a tuple: {friends}\n")
 
 print("The first friend is", friends[0])
 
@@ -18,14 +18,14 @@ try:
 except TypeError:
     print("Tuples are immutable!")
 
-# a single-element tuple
+# a single-element tuple, parenthesis are not mandatory here!
 best_friend = ("joe",)
-print(best_friend)
+print("A tuple with a single element:", best_friend)
 
 # assign a new tuple
 best_friend = ("tim", )
-print(best_friend)
+print("tuples are immutable, but a variable can be reassigned:", best_friend)
 
 # tuple comprehension
 squared_evens = tuple(x**2 for x in range(10) if x % 2 == 0)
-print(squared_evens)
+print("Not really a comprehension, but close enough:", squared_evens)
