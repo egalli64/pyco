be8d696b5167559efa5b987f75ecf2d6f7864f9d
Emanuele <egalli64@gmail.com>
2024-01-22 12:33:38 +0100

    precedence

---
+++ p1/m2_number/s5_precedence/e1_example.py
@@ -0,0 +1,14 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 2 - Numbers and operators
+
+Operator precedence and association
+"""
+# by default, multiply first
+print(2 + 6 * 7 - 3)  # 41
+
+# rearrange in the preferred way
+print((2 + 6) * (7 - 3))  # 32
---
+++ p1/m2_number/s5_precedence/e2_exercise.py
@@ -0,0 +1,14 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 2 - Numbers and operators
+
+Operator precedence and association
+"""
+# TODO: use parentheses to fix the bug
+x = 1.0
+result = 1 / x + 1 / x + 1 / x + 1 / x
+print("Actual result is:", result)
+print("Expected result is: 0.6000000000000001")
