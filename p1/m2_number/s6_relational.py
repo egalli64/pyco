a457d39ac017f61ae8623c0cd7e87b97fc3c5dad
Emanuele <egalli64@gmail.com>
2024-01-22 12:42:43 +0100

    operators

---
+++ p1/m2_number/s6_relational.py
@@ -0,0 +1,47 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 2 - Numbers and operators
+
+Relational operators
+"""
+
+alpha = 12
+beta = 21
+gamma = 12
+print(f"alpha is {alpha}, beta is {beta}, gamma is {gamma}")
+
+result = alpha < beta  # True
+print("alpha < beta?", result)
+
+result = alpha < gamma  # False
+print("alpha < gamma?", result)
+
+result = alpha <= gamma  # True
+print("alpha <= gamma?", result)
+
+result = alpha > beta  # False
+print("alpha > beta?", result)
+
+result = alpha > gamma  # False
+print("alpha > gamma?", result)
+
+result = alpha >= gamma  # True
+print("alpha >= gamma?", result)
+
+result = alpha == beta  # False
+print("alpha == beta?", result)
+
+result = alpha == gamma  # True
+print("alpha == gamma?", result)
+
+result = alpha != beta  # True
+print("alpha != beta?", result)
+
+result = alpha != gamma  # False
+print("alpha != gamma?", result)
+
+f_alpha = 12.0
+print("alpha == f_alpha?", alpha == f_alpha)
