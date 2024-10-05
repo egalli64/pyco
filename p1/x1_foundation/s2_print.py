9aff7bda4e8996be0f6b656f54bbec40534cb765
Emanuele <egalli64@gmail.com>
2024-10-05 16:54:15 +0200

    print

---
+++ p1/x1_foundation/s2_print.py
@@ -0,0 +1,26 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 1 - Foundation
+
+Write to standard output (console)
+"""
+
+# 1. plain print
+print("This is a single string: Hello, Python!")
+
+# 2. print more arguments
+print("First argument", "Second argument")
+
+# 3. use a give separator between arguments
+print("First argument", "Second argument", sep="_")
+
+# 4. by default the termination ("end") is a newline
+# Get rid of it
+print("Hello, ", end="")
+print("Python!", end="")
+
+# 5. print just a newline
+print()
