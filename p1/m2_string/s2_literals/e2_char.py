89ab7197bbb2a7f015a20ae519b167ccfac24cf8
Emanuele <egalli64@gmail.com>
2024-10-07 22:10:43 +0200

    review

--- p1/m2_string/s2_literals/e2_char.py
+++ p1/m2_string/s2_literals/e2_char.py
@@ -8,19 +8,20 @@ Module 2 - String
 String literals
 Single char
 """
+
 # a Python char is a string sized 1
-s4 = "x"
-print(type(s4), s4)
+s_x = "x"
+print(type(s_x), s_x, "- no char type in Python, just a string sized one")
 
 # get the Unicode code point representation of a char
-code_x = ord(s4)
-print(type(code_x), code_x)
+code_x = ord(s_x)
+print(type(code_x), code_x, "- Unicode")
 
 # the integer representation for the Unicode code point of a grinning face
 code_grin = 0x1F600
 s5 = chr(code_grin)
-print("A Unicode code and its associated character:", hex(code_grin), s5)
+print("Grin hex Unicode and its character representation:", hex(code_grin), s5)
 
 # escape sequences
-s6 = "a multiline string\nis 'useful' to represent \"long\" text\nthat should be read by humans"
+s6 = "a multiline string\n\tis 'useful' to represent \"long\" text\nthat should be read by humans"
 print(s6)
