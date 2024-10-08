a19787e8362ce67f7362eacc8287f6e77e34ae88
Emanuele <egalli64@gmail.com>
2024-10-08 22:01:06 +0200

    review

--- p1/m2_string/s3_literals/e3_fstring.py
+++ p1/m2_string/s3_literals/e3_fstring.py
@@ -8,8 +8,16 @@ Module 2 - String
 String literals
 F-string
 """
+
 PI = 3.141592653589793
 radius = 5
 
+# referring to variable / expression in a f-string
 print(f"The area of a circle with radius {radius} is {PI * radius ** 2}")
+
+# real number formatting
 print(f"A pi approximation: {PI:.5f}")
+
+# padding and alignment
+print(f"Left aligned:  _{PI:<20}_")
+print(f"Right aligned: _{PI:>20}_")
