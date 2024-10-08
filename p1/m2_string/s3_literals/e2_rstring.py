a5626cb1e23279b537b2cc275f44451dcbd8df81
Emanuele <egalli64@gmail.com>
2024-10-08 22:30:14 +0200

    raw string

---
+++ p1/m2_string/s3_literals/e2_rstring.py
@@ -0,0 +1,18 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 2 - String
+
+String literals
+Raw string
+"""
+
+# Windows file path
+win_path = r"C:\temp\logs\next"
+print("On Windows, the new log path for the next module is:", win_path)
+
+# Regular expressions
+regex = r"\d{2}-\d{3}"
+print("A regular expression:", regex)
