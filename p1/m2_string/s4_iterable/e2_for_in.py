093ba7e89ff2582290930e5a21927206102d7415
Emanuele <egalli64@gmail.com>
2024-10-10 22:06:16 +0200

    for in

---
+++ p1/m2_string/s4_iterable/e2_for_in.py
@@ -0,0 +1,16 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 2 - String
+
+Iterable - Loop by for-in
+"""
+
+# a string is an iterable
+str = "Python"
+print(f"For-in loop on '{str}':")
+
+for c in str:
+    print(c)
