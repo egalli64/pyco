8c0d2d350a2a105ae491804a239ca2faf37fe309
Emanuele <egalli64@gmail.com>
2024-10-20 19:34:16 +0200

    review

--- p1/m2_string/s5_sequences/e8_del.py
+++ p1/m2_string/s5_sequences/e8_del.py
@@ -7,13 +7,10 @@ Module 2 - String
 
 Sequences - del statement
 """
+
 # a string is a sequence
 str = "welcome to Python's world"
 print("A string:", str)
-# a list is a sequence, too
-lis = ["bob", "tom", "kim", "tim"]
-print("A list:", lis)
-print()
 
 # strings are immutable sequences, do not support del on their elements
 try:
@@ -22,10 +19,13 @@ except TypeError as ex:
     print(ex)
 
 # lists are mutable sequences, do support del on their elements
-del lis[2]
-print(f"After del on the mutable sequence: {lis}\n")
+lis = ["bob", "tom", "kim", "tim"]
+print("\nA list:", lis)
 
-print("Be careful with index value:", end=" ")
+del lis[2]
+print("After deleting element in position 2:", lis)
+
+print("\nBe careful with index value:", end=" ")
 try:
     del lis[42]
 except IndexError as ex:
