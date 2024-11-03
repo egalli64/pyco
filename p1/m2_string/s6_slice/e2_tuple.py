6f1e136f72dc657b24e906344218a441b017545a
Emanuele <egalli64@gmail.com>
2024-11-03 19:20:37 +0100

    slice on tuple

---
+++ p1/m2_string/s6_slice/e2_tuple.py
@@ -0,0 +1,32 @@
+"""
+Python Course - Part 1
+
+https://github.com/egalli64/pyco
+
+Module 2 - String
+
+Sequences - slice on tuple
+"""
+
+# a tuple
+friends = ("bob", "tom", ["kim", "karen", "krug"], "abe", "luc")
+print("A list:", friends)
+print()
+
+# A slice with start and stop: tom, [...]
+print("Plain slice [1..3):", end=" ")
+slice = friends[1:3]
+print(slice)
+
+# Can't assign a new reference to a tuple element
+try:
+    slice[0] = "tim"
+except:
+    print("Assign to a tuple leads to a TypeError")
+
+# A list is mutable, even if it is defined in a tuple
+slice[1][0] = "kimi"
+print("Changing kim to kimi:", slice)
+
+# No effect on the original tuple
+print("The change is seen also in the original tuple!", friends[2])
