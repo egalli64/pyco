62965988145af590861efbaa7ae5aafc6ea2aded
Emanuele <egalli64@gmail.com>
2024-12-10 19:17:04 +0100

    review

--- p2/m2_list/s3_editing.py
+++ p2/m2_list/s3_editing.py
@@ -7,41 +7,43 @@ Module 2 - List
 
 List methods for editing
 """
+
 friends = ["bob", "tom", "kim", "abe", "luc"]
 print("A list:", friends)
 
-# add 'joe' as new last element
+# append a new element (at the end)
 friends.append("joe")
-print("Appended joe:", friends)
+print("After append:", friends)
 
-# insert 'lee' at index 3
+# insert in the given position
 friends.insert(3, "lee")
-print("Insert at 3:", friends)
+print("After insert at 3:", friends)
 
+# extend with an iterable
 friends.extend(("ada", "ben", "cho"))
-print("Extended:", friends)
+print("After extend:", friends)
 
 # concatenation among different types doesn't compile
 # friends = friends + ("dean", "emma", "fin")
 # augmented concatenated assignment works alright
 friends += "dean", "emma", "fin"
-print("Extended by concatenation:", friends)
+print("After concatenation:", friends)
 
-# pop the second element from the end
+# delete the element in a given position
 del friends[-2]
 print("After del at -2:", friends)
 
-# po the last element in the list
+# pop the last element
 popped = friends.pop()
-print("Pop at the end:", popped)
+print("The element popped:", popped)
 print("After pop:", friends)
 
-# po the second element from the list
+# pop in a given position
 popped = friends.pop(1)
-print("Pop at position 1:", popped)
+print("The element popped at position 1:", popped)
 print("After pop:", friends)
 
-# a bad index raises IndexError
+# pop a bad index raises an IndexError
 try:
     friends.pop(42)
 except IndexError as ex:
@@ -51,8 +53,13 @@ except IndexError as ex:
 friends.remove("abe")
 print("After removing abe:", friends)
 
+# remove a bad value raises a ValueError
 try:
     print("Trying to remove 'abe' a second time ...", end=" ")
     friends.remove("abe")
 except ValueError as ex:
     print(ex, "... where x stands for 'abe'")
+
+# make the list empty
+friends.clear()
+print("After clear:", friends)
