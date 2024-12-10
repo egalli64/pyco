e973aea8855d51f18bc6a15062927174fc21ec7d
Emanuele <egalli64@gmail.com>
2024-12-10 15:08:47 +0100

    loop else

---
+++ p2/m1_control_flow/s5_loop_else/e2_no_break.py
@@ -0,0 +1,18 @@
+"""
+Python Course - Part 2
+
+https://github.com/egalli64/pyco
+
+Module 1 - Control Flow
+
+Alterate / terminate a loop
+The loop - else statement
+"""
+
+s = "Welcome To Pythonville"
+print(f"Working on string '{s}'\n")
+
+for c in s:
+    print(c, end=" ")
+else:
+    print("\nAll characters processed")
