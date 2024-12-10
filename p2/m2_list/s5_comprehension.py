07ba987dce123f47c4d753dea59b6ce689f80c7f
Emanuele <egalli64@gmail.com>
2024-12-10 20:27:22 +0100

    review

--- p2/m2_list/s5_comprehension.py
+++ p2/m2_list/s5_comprehension.py
@@ -7,9 +7,9 @@ Module 2 - List
 
 List comprehension
 """
+
 # list of values in [1 .. 6]
-dice = list(range(1, 7))
-print("A list from a range:", dice)
+print("A list from a range:", list(range(1, 7)))
 
 
 # list of squares
@@ -21,16 +21,13 @@ print("Populating a list with a for loop:", squares)
 # list comprehension
 
 # worst than the list-range approach, actually
-dice_c = [x for x in range(1, 7)]
-print("A simple but useless comprehension example:", dice)
+print("A simple but useless comprehension example:", [x for x in range(1, 7)])
 
 # this one is more interesting
-squares_c = [x**2 for x in range(1, 11)]
-print("Compacting list creation and population in a comprehension:", squares_c)
+print("Compacting list creation and population in a comprehension:", [x**2 for x in range(1, 11)])
 
 # filter to get only even numbers
-evens = [x for x in range(10) if x % 2 == 0]
-print("A comprehension with conditional:", evens)
+print("A comprehension with conditional:", [x for x in range(10) if x % 2 == 0])
 
 # the external comprehension for rows, the internal one for columns
 matrix = [[i + j for j in range(2)] for i in range(3)]
