aaad9468392a26890452441648df3e2cd5523dc3
Emanuele <egalli64@gmail.com>
2024-12-10 11:52:54 +0100

    review

--- p2/m1_control_flow/s3_loop/e1_while.py
+++ p2/m1_control_flow/s3_loop/e1_while.py
@@ -7,14 +7,15 @@ Module 3 - Control Flow
 
 While loop
 """
-print("in [0..10):", end="\t")
+
+print("Print while in [0..10): ", end="\t")
 i = 0
 while i < 10:
     print(i, end=" ")
     i += 1
 print()
 
-print("in [5..10):", end="\t")
+print("Print while in [5..10): ", end="\t")
 i = 5
 while i < 10:
     print(i, end=" ")
@@ -22,7 +23,7 @@ while i < 10:
 print()
 
 # skip odd values
-print("even [0..10):", end="\t")
+print("Print even while in [0..10):", end="\t")
 i = 0
 while i < 10:
     print(i, end=" ")
@@ -32,7 +33,7 @@ print("\n")
 # loop on a string
 s = "Python"
 
-print(f"While-looping on each char in {s}:", end=" ")
+print(f"While-loop on each char in {s}:", end=" ")
 i = 0
 while i < len(s):
     print(s[i], end=" ")
