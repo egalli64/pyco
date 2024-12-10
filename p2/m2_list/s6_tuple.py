600bed963df15b3f396a12633891829bdadd8166
Emanuele <egalli64@gmail.com>
2024-12-10 21:12:03 +0100

    review

--- p2/m2_list/s6_tuple.py
+++ p2/m2_list/s6_tuple.py
@@ -7,12 +7,15 @@ Module 2 - List
 
 Tuple
 """
+
 # a tuple
 friends = ("bob", "tom", "kim", "abe", "luc")
 print(f"friends in a tuple: {friends}\n")
 
 print("The first friend is", friends[0])
 
+# mutable functionality are not available
+print("Can't change elements in a tuple:", end=" ")
 try:
     friends[0] = "bill"
 except TypeError:
@@ -23,7 +26,7 @@ best_friend = ("joe",)
 print("A tuple with a single element:", best_friend)
 
 # assign a new tuple
-best_friend = ("tim", )
+best_friend = ("tim",)
 print("tuples are immutable, but a variable can be reassigned:", best_friend)
 
 # tuple comprehension
