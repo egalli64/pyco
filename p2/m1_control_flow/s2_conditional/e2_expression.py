172c0a559f8d0d7ba9f1b6d7a871f5a11c344a8f
Emanuele <egalli64@gmail.com>
2024-12-10 11:36:14 +0100

    comparision if-else expression/statement

--- p2/m1_control_flow/s2_conditional/e2_expression.py
+++ p2/m1_control_flow/s2_conditional/e2_expression.py
@@ -7,11 +7,20 @@ Module 3 - Control Flow
 
 Conditional expression
 """
+
 import random
 
 # a float in [0.0 .. 1.0)
 value = random.random()
 print(f"Different behavior accordingly to the random value {value}\n")
 
+# using the conditional expression
 description = "low" if value < 0.5 else "high"
+print(f"The value is {description}")
+
+# same, but using a plain if - else
+if value < 0.5:
+    description = "close enough to zero"
+else:
+    description = "close enough to one"
 print(f"The value is {description}")
