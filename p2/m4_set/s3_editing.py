c074a8a938ce214c269269194ab091f6a13b54a3
Emanuele <egalli64@gmail.com>
2024-12-12 10:57:06 +0100

    review

--- p2/m4_set/s3_editing.py
+++ p2/m4_set/s3_editing.py
@@ -1,5 +1,5 @@
 """
-Python Course - Part 1
+Python Course - Part 2
 
 https://github.com/egalli64/pyco
 
@@ -7,21 +7,23 @@ Module 4 - Set
 
 Set: add, remove, ...
 """
+
 # a set
 friends = {"bob", "tom", "kim"}
 print("Friends:", friends)
 
 # add an element
 friends.add("mia")
-friends.add("mia")
+# no effect - duplicate is silently discarded
+friends.add("bob")
 print("Friends (add):", friends)
 
-# update a set with an iterable
+# update a set with an iterable, tom is not inserted being already in
 others = ["tom", "zoe"]
 friends.update(others)
 print("Friends (update):", friends)
 
-# remove an element
+# remove an element, or throw a KeyError
 try:
     friends.remove("tom")
     print("Friends (remove):", friends)
@@ -29,17 +31,20 @@ try:
 except KeyError as ex:
     print("Can't remove a missing element:", ex)
 
-# discard an element
+# discard an element, or ignore the request
 friends.discard("kim")
 friends.discard("kim")
 print("Friends (discard):", friends)
 
-# pop an element
-while len(friends):
-    cur = friends.pop()
-    print(cur, "was a friend (pop)")
+# pop an element (chosen by set)
+print(friends.pop(), "was a friend (pop)")
 print("Friends (pop):", friends)
 
+# make the set empty
+friends.clear()
+print("Friends (clear):", friends)
+
+# can't pop on an empty set
 try:
     friends.pop()
 except KeyError as ex:
