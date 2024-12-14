4b7a5e4edb177ca3657c9d92780a931022176ad6
Emanuele <egalli64@gmail.com>
2024-12-14 10:53:29 +0100

    split and review

---
+++ p4/m2_structured/s2_CSV/e1_writerow.py
@@ -0,0 +1,25 @@
+"""
+Python Course - Part 4
+
+https://github.com/egalli64/pyco
+
+Module 2 - Structured File
+
+CSV - writerow
+"""
+
+import csv
+
+FILENAME = "friends.csv"
+HEADER = ["Name", "Age", "City"]
+
+friends = ["Tom", "Jenny", "Bob", "Kim", "Micky", "Lee"]
+ages = [42, 37, 32, 41, 43, 38]
+cities = ["Madrid", "Amsterdam", "Paris", "Stockholm", "Berlin", "London"]
+
+# write as CSV the values passed, row by row
+with open(FILENAME, "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(HEADER)
+    for i in range(len(friends)):
+        writer.writerow([friends[i], ages[i], cities[i]])
