97d4284b369796a46724892286b57c96a6e2ca00
Emanuele <egalli64@gmail.com>
2024-12-19 16:59:40 +0100

    fix mixup

--- p1/m4_control_flow/s3_chained_comp.py/e1_in_interval.py
+++ p1/m4_control_flow/s3_chained_comp.py/e1_in_interval.py
@@ -5,12 +5,16 @@ https://github.com/egalli64/pyco
 
 Module 4 - Control Flow
 
-Chained comparisons - monotonic triplet
+Chained comparisons - in interval
 """
 
-for xs in (3, 7, 8), (3, 8, 7), (7, 8, 9):
-    print("(plain) {xs} is monotonic", end=": ")
-    print(xs[0] <= xs[1] and xs[1] <= xs[2], end=" ... ")
+a = 3
+b = 10
 
-    print("(chained)", end=": ")
-    print(xs[0] <= xs[1] <= xs[2])
+print(f"Given the interval [{a}..{b}], check if x is in it")
+for x in (3, 7, 11):
+    print("(plain) x is", x, end=": ")
+    print(a <= x and x <= b, end="\t... ")
+
+    print("(chained) x is", x, end=": ")
+    print(a <= x <= b)
