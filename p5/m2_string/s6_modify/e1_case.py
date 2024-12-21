88dc8a258e9e7e90b002c7d3f9b4bf2ab164c4d5
Emanuele <egalli64@gmail.com>
2024-12-21 15:48:39 +0100

    Modify - case

---
+++ p5/m2_string/s6_modify/e1_case.py
@@ -0,0 +1,18 @@
+"""
+Python Course - Part 5
+
+https://github.com/egalli64/pyco
+
+Module 2 - String
+
+Modify - case
+"""
+
+s = "welcome to Python world"
+print("The original string:", s)
+
+print("Upper:", s.upper())
+print("Lower:", s.lower())
+print("Title:", s.title())
+print("Capitalize:", s.capitalize())
+print("Swap case:", s.swapcase())
