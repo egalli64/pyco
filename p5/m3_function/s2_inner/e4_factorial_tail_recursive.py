a1f8511af1edd1a5439a16fbb97f687e9bf344b3
Emanuele <egalli64@gmail.com>
2024-12-21 19:25:40 +0100

    Split inner function - no inner, tail recursive

---
+++ p5/m3_function/s2_inner/e4_factorial_tail_recursive.py
@@ -0,0 +1,20 @@
+"""
+Python Course - Part 5
+
+https://github.com/egalli64/pyco
+
+Module 3 - Function
+
+Inner function - no inner, tail recursive
+"""
+
+
+def factorial(n, accumulator=1):
+    """Tail recursive implementation - exposing the accumulator to the caller"""
+    if n == 0:
+        return accumulator
+    else:
+        return factorial(n - 1, n * accumulator)
+
+
+print("Factorial of 7 (tail recursive):", factorial(7))
