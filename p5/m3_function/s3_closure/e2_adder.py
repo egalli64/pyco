b0af743444433259287ef34e3f5f4513f90d6337
Emanuele <egalli64@gmail.com>
2024-12-22 09:27:47 +0100

    Closure - Adder example

---
+++ p5/m3_function/s3_closure/e2_adder.py
@@ -0,0 +1,27 @@
+"""
+Python Course - Part 5
+
+https://github.com/egalli64/pyco
+
+Module 3 - Function
+
+Closure - Adder example
+"""
+
+
+def adder(fixed):
+    """Generate a closure, adder to the passed argument"""
+
+    def add(second):
+        """Accept the second operand and return the sum with the fixed one"""
+        return fixed + second
+
+    return add
+
+
+# Create an adder closure based on 12
+adder_12 = adder(12)
+
+print("Passing different values to the 'adder 12' closure:")
+print("Argument 30:", adder_12(30))
+print("Argument -7:", adder_12(-7))
