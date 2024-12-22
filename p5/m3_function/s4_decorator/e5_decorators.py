3586ebc740429c9c87d52a8bb6894bb5f48f3edf
Emanuele <egalli64@gmail.com>
2024-12-22 11:14:39 +0100

    Decorator - apply more than one decorator

---
+++ p5/m3_function/s4_decorator/e5_decorators.py
@@ -0,0 +1,40 @@
+"""
+Python Course - Part 5
+
+https://github.com/egalli64/pyco
+
+Module 3 - Function
+
+Decorator - apply more than one decorator
+"""
+
+
+def uppercase_decorator(func):
+    """Convert the result of the decorated function to uppecase"""
+
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result.upper()
+
+    return wrapper
+
+
+def exclaim_decorator(func):
+    """Append an exclamation mark to the decorated function result"""
+
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return result + "!"
+
+    return wrapper
+
+
+@uppercase_decorator
+@exclaim_decorator
+def greet_generator(name):
+    return f"Hello, {name}"
+
+
+name = "Bob"
+print("Generate a exclaim- and uppercase- decorated greet for", name)
+print(greet_generator(name))
