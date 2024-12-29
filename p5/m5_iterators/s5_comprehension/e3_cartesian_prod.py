22206e12b2a651bd6ab917c0fae41e791e9440ae
Emanuele <egalli64@gmail.com>
2024-12-29 18:22:05 +0100

    readability

--- p5/m5_iterators/s5_comprehension/e3_cartesian_prod.py
+++ p5/m5_iterators/s5_comprehension/e3_cartesian_prod.py
@@ -8,16 +8,25 @@ Module 5 - Iterators
 Generator and list comprehension
 List comprehension for Cartesian product
 """
+
 names = ["bob", "kim"]
 colors = ["blue", "red", "green"]
 
-# cartesian product of names and colors
-couples = [(name, color) for name in names for color in colors]
-print(couples)
+print("Names:", names)
+print("Colors:", colors)
+print()
 
-# same, by double for loop
+# 1. no comprehension
+print("Cartesian product of names and colors (by for-in):")
+
 couples = []
 for name in names:
     for color in colors:
         couples.append((name, color))
+print(couples)
+
+# 2. comprehension
+print(34 * " " + "(comprehension):")
+
+couples = [(name, color) for name in names for color in colors]
 print(couples)
