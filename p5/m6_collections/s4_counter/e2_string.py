4b233917f82bab7cfd2a60b450641efe502f0211
Emanuele <egalli64@gmail.com>
2024-12-31 20:29:48 +0100

    counter from string

---
+++ p5/m6_collections/s4_counter/e2_string.py
@@ -0,0 +1,15 @@
+"""
+Python Course - Part 5
+
+https://github.com/egalli64/pyco
+
+Module 6 - The collections module
+
+Counter - on a string
+"""
+
+from collections import Counter
+
+s = "welcome to python world"
+counter = Counter(s)
+print(counter)
