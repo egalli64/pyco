f503066e255c31d0a4703e943b22823579f951cd
Emanuele <egalli64@gmail.com>
2025-02-22 14:52:31 +0100

    close in finally

--- p4/m3_rdb/s2_sqlite3_connection/e1_safe.py
+++ p4/m3_rdb/s2_sqlite3_connection/e1_safe.py
@@ -10,13 +10,17 @@ sqlite3 - check exception on connect
 
 import sqlite3
 
-for path in ["p4/missing/countries.db", 42]:
+for path in ["p4/missing/countries.db", "p4/m3_rdb/countries.db", 42]:
+    conn = None
     try:
         print("Trying to connect to", path)
-        # implicit close() on conn
-        with sqlite3.connect(path) as conn:
-            pass
+        conn = sqlite3.connect(path)
+        print("Connected to", path)
     except sqlite3.Error as ex:
         print("SQLite Error:", ex)
     except Exception as ex:
         print("Something went wrong:", ex)
+    finally:
+        if conn:
+            print("Closing connection to", path)
+            conn.close()
