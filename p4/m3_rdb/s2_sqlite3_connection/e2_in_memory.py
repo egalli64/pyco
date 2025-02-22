86e481351b6f77914f0dcab32b8d91dee91b377a
Emanuele <egalli64@gmail.com>
2025-02-22 11:00:17 +0100

    in-memory

---
+++ p4/m3_rdb/s2_sqlite3_connection/e2_in_memory.py
@@ -0,0 +1,22 @@
+"""
+Python Course - Part 4
+
+https://github.com/egalli64/pyco
+
+Module 3 - RDB
+
+sqlite3 - connect to an in-memory database
+"""
+
+import sqlite3
+
+# lack of robustness and persistency
+# in-memory database, not using "with" nor checking for exception
+conn = sqlite3.connect(":memory:")
+
+print("connected to in-memory database")
+
+# no "with" means explicit call to close() under programmer responsibility
+conn.close()
+
+print("disconnected to in-memory database")
