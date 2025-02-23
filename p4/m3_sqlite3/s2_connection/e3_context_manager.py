5ccceca5d22406df2a37216859ff75a338881d83
Emanuele <egalli64@gmail.com>
2025-02-23 16:46:22 +0100

    more details

--- p4/m3_sqlite3/s2_connection/e3_context_manager.py
+++ p4/m3_sqlite3/s2_connection/e3_context_manager.py
@@ -10,19 +10,19 @@ context manager on connection is not meant to close it
 
 import sqlite3
 
+# the connection is created in a context manager, but close() is still our responsibility!
 with sqlite3.connect("p4/m3_sqlite3/countries.db") as conn:
-    cursor = conn.cursor()
     # this should work fine
-    conn.execute("SELECT 1")
+    print("Check ...", conn.execute("SELECT 1").fetchone()[0])
 
 # since exiting the context manager DO NOT CLOSE the connection, this should work fine
-conn.execute("SELECT 2")
+print("Check ...", conn.execute("SELECT 2").fetchone()[0])
 
 # EXPLICIT CLOSE of the connection is REQUIRED
 conn.close()
 
 try:
     # this call should throw an exception
-    conn.execute("SELECT 3")
+    print("Check ...", conn.execute("SELECT 3").fetchone()[0])
 except sqlite3.ProgrammingError as ex:
     print("Now the connection is closed:", ex)
