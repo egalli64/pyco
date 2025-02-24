679f0bf032ea3198c969216fbed413a60fb3aa2b
Emanuele <egalli64@gmail.com>
2025-02-24 12:53:47 +0100

    Connection (raw)

---
+++ p4/m4_SQLAlchemy/s2/e2_connection.py
@@ -0,0 +1,32 @@
+"""
+Python Course - Part 4
+
+https://github.com/egalli64/pyco
+
+Module 4 - SQLAlchemy
+
+Get a Connection (raw)
+"""
+
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import SQLAlchemyError
+
+DB_PATH = "sqlite://"
+
+# 1. create an engine
+engine = create_engine(DB_PATH)
+
+# 2. get a connection
+conn = engine.connect()
+
+# 3. use the connection
+print("Can execute:", conn.execute(text("SELECT 3")).fetchone()[0])
+
+# 4. close the connection
+conn.close()
+
+# 5. now the connection is unusable
+try:
+    conn.execute(text("SELECT 5"))
+except SQLAlchemyError as ex:
+    print("Can't execute:", ex)
