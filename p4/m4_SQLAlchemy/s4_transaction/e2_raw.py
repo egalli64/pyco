092363693380d50b359e77be3b9549a6a27878cc
Emanuele <egalli64@gmail.com>
2025-02-25 15:04:16 +0100

    explicit commit / rollback

---
+++ p4/m4_SQLAlchemy/s4_transaction/e2_raw.py
@@ -0,0 +1,37 @@
+"""
+Python Course - Part 4
+
+https://github.com/egalli64/pyco
+
+Module 4 - SQLAlchemy
+
+Transaction: in a raw way
+"""
+
+from sqlalchemy import create_engine, text
+
+DB_PATH = "sqlite:///p4/m4_SQLAlchemy/countries.db"
+engine = create_engine(DB_PATH)
+conn = engine.connect()
+
+try:
+    # start a transaction
+    tx = conn.begin()
+
+    # show the initial status
+    print(conn.execute(text("SELECT * FROM region")).fetchall())
+
+    # a change that requires a commit
+    result = conn.execute(text("INSERT INTO region (name) values ('Antarctica')"))
+    print(f"Inserted with PK {result.lastrowid}")
+
+    # show the final status
+    print(conn.execute(text("SELECT * FROM region")).fetchall())
+
+    tx.commit()
+except:
+    print(f"Insert of row with PK {result.lastrowid} rolled back!")
+    tx.rollback()
+
+# closing
+conn.close()
