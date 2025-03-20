32f0fe960da14777b356fb26459f1dc84b6cb90e
Emanuele <egalli64@gmail.com>
2025-03-20 22:24:22 +0100

    a matrix

---
+++ p2/m1_list/s3_definition/e3_matrix.py
@@ -0,0 +1,13 @@
+"""
+Python Course - Part 2
+
+https://github.com/egalli64/pyco
+
+Module 1 - List
+
+Matrix literal definition
+"""
+
+xs = [[1, 2, 3], [3, 4, 5], [6, 7, 8]]
+print(xs)
+print("Second list, third element:", xs[1][2])
