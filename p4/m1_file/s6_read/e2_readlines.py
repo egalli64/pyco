89c4822c50c8db4c6881c2148cc55cb82c3d66e5
Emanuele <egalli64@gmail.com>
2025-03-22 10:45:04 +0100

    readability

--- p4/m1_file/s6_read/e2_readlines.py
+++ p4/m1_file/s6_read/e2_readlines.py
@@ -5,13 +5,13 @@ https://github.com/egalli64/pyco
 
 Module 1 - File
 
-Read
+Read by readlines(): all file in a list of strings
 """
 
 FRIENDS_FILENAME = "p4/m1_file/friends.txt"
 
 with open(FRIENDS_FILENAME) as f:
-    print("Read from file (full - in a list of strings):")
+    print("Read all lines from file:")
     # readlines do not strip the tailing newline
     lines = f.readlines()
 
