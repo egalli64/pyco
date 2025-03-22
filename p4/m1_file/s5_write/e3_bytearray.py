c45bfc7754b09016f3aef9ff53fc57f06ce74254
Emanuele <egalli64@gmail.com>
2025-03-22 21:35:41 +0100

    review

--- p4/m1_file/s5_write/e3_bytearray.py
+++ p4/m1_file/s5_write/e3_bytearray.py
@@ -8,6 +8,19 @@ Module 1 - File
 Write bytes in a bytearray to binary file by write()
 """
 
-data = bytearray([0, 1, 127, 255])
+# an array of small integers - interval [0 .. 255]
+data = [0, 1, 127, 255]
+print("Serializing:", data)
+
 with open("data.bin", "wb") as f:
-    f.write(data)
+    encoded = bytearray(data)
+    f.write(encoded)
+
+# a string - how the characters are actually represented is an internal detail
+hello = "こんにちは、世界"
+print("Serializing:", hello)
+
+with open("hello_world.bin", "wb") as f:
+    # use the standard UTF-8 encoding format
+    encoded = hello.encode("utf-8")
+    f.write(encoded)
