144f4150c53303e3a12744c4e0e35954d9e4d41d
Emanuele <egalli64@gmail.com>
2025-03-23 23:21:58 +0100

    readability

--- p4/m1_file/s6_read/e6_bytes.py
+++ p4/m1_file/s6_read/e6_bytes.py
@@ -5,15 +5,15 @@ https://github.com/egalli64/pyco
 
 Module 1 - File
 
-Read bytes in a bytearray from binary file by read()
+Read bytes from binary file by read()
 """
 
-# deserialize a byte array
+# deserialize bytes
 with open("data.bin", "rb") as f:
     data = f.read()
     decoded = list(data)
 
-print(f"Deserializing {data} (bytearray)")
+print(f"Deserializing {data} (bytes)")
 print("As list of (small) integers:", list(data))
 
 # deserialize a UTF-8 string
@@ -21,5 +21,5 @@ with open("hello_world.bin", "rb") as f:
     data = f.read()
     decoded = data.decode("utf-8")
 
-print(f"Deserializing {data} (bytearray)")
+print(f"Deserializing {data} (bytes)")
 print("As string:", decoded)
