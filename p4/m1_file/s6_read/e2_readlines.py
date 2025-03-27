50635af2dd216da979d1d70e011bcc9a9f302796
Emanuele <egalli64@gmail.com>
2025-03-27 20:44:32 +0100

    readability

--- p4/m1_file/s6_read/e2_readlines.py
+++ p4/m1_file/s6_read/e2_readlines.py
@@ -11,10 +11,10 @@ Read by readlines(): all file in a list of strings
 FRIENDS_FILENAME = "p4/m1_file/friends.txt"
 
 with open(FRIENDS_FILENAME) as f:
-    print("Read all lines from file:")
+    print("Reading all lines from file", end=" ... ")
     # readlines do not strip the tailing newline
     lines = f.readlines()
 
+print(f"they are {len(lines)}:")
 for line in lines:
     print(line, end="")
-print("---\n")
