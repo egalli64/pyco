e1a0e3b68fc0cdeb7dfa9cf001789451194495f0
Emanuele <egalli64@gmail.com>
2025-03-27 13:45:46 +0100

    fix naming

--- p5/m5_iterators/s3_generator/e7_even_squared_expression.py
+++ p5/m5_iterators/s3_generator/e7_even_squared_expression.py
@@ -5,17 +5,15 @@ https://github.com/egalli64/pyco
 
 Module 5 - Iterators
 
-Iterators - Generator
-
-Using a generator by comprehension
+Using a generator by expression
 """
 
-print("Even squared in [0 .. 10) by comprehension:", end=" ")
+print("Even squared in [0 .. 10) by expression:", end=" ")
 for cur in (n**2 for n in range(10) if n % 2 == 0):
     print(cur, end=" ")
 print()
 
-print("Even squared in [-3 .. 8) by comprehension:", end=" ")
+print("Even squared in [-3 .. 8) by expression:", end=" ")
 for cur in (n**2 for n in range(-3, 8) if n % 2 == 0):
     print(cur, end=" ")
 print()
