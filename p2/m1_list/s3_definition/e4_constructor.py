a9ab054eef433856f6a9a9e9b44b051d6e3dfb7f
Emanuele <egalli64@gmail.com>
2025-03-28 20:28:45 +0100

    more details

--- p2/m1_list/s3_definition/e4_constructor.py
+++ p2/m1_list/s3_definition/e4_constructor.py
@@ -16,8 +16,25 @@ print("A list:", friends)
 others = list(friends)
 print("Another list:", others)
 
+# the copy creates a new list
 if id(friends) != id(others):
     print("The two variables reference two different objects")
 
+# but the elements are the same!
 if id(friends[0]) == id(others[0]):
     print("But the elements are referring to the same objects!")
+
+# comparing the two lists for equality gives True
+if friends == others:
+    print("The two list are equal")
+else:
+    print("Unexpected")
+
+# Since the elements are here immutable values, it's OK sharing objects
+friends[0] = "bobby"
+print(f"After changing a reference in {friends}, {others} stays unchanged")
+
+if friends != others:
+    print("Now the two lists are different")
+else:
+    print("Unexpected")
