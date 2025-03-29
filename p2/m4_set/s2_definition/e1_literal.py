fae312b0458323d0dd1c22cc96382b49bbfdea20
Emanuele <egalli64@gmail.com>
2025-03-29 11:31:38 +0100

    split and review

---
+++ p2/m4_set/s2_definition/e1_literal.py
@@ -0,0 +1,21 @@
+"""
+Python Course - Part 2
+
+https://github.com/egalli64/pyco
+
+Module 4 - Set
+
+Definition - literal (when possible)
+"""
+
+# a literal set
+friends = {"bob", "tom", "kim"}
+print("A literal set:", friends)
+
+# Oh, no! this is an empty _dictionary_!
+dict = {}
+print("This is _not_ a set:", dict, type(dict))
+
+# an empty set by constructor
+enemies = set()
+print("An empty set by constructor:", enemies, type(enemies))
