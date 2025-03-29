0fe7b57a28597b76fc761d8842fb27cbb3ce72fd
Emanuele <egalli64@gmail.com>
2025-03-29 13:20:04 +0100

    split and review

---
+++ p2/m4_set/s3_read/e3_len.py
@@ -0,0 +1,17 @@
+"""
+Python Course - Part 2
+
+https://github.com/egalli64/pyco
+
+Module 4 - Set
+
+Using the built-in len()
+"""
+
+# an iterable (a string)
+name = "emanuele"
+print("Any iterable support len:", len(name))
+
+# a set
+letters = set(name)
+print(f"This set '{letters}' has len {len(letters)}")
