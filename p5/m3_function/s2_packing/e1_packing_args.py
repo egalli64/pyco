d7152b7db930d23b87fd54ec2b06055cd6aa1de4
Emanuele <egalli64@gmail.com>
2025-04-02 10:50:44 +0200

    clarify

--- p5/m3_function/s2_packing/e1_packing_args.py
+++ p5/m3_function/s2_packing/e1_packing_args.py
@@ -1,38 +1,42 @@
 """
-Python Course - Part 3
+Python Course - Part 5
 
 https://github.com/egalli64/pyco
 
-Module 1 - Function
+Module 3 - Function
 
 Parameters and arguments - packing args
 """
 
 
 def greeting(salutation, *args):
-    """Packing arguments to args"""
+    """Packing arguments to the tuple parameter args"""
     print(salutation, end=" ")
+
     if not args:
+        # nothing provided as args, using a default value
         print("stranger, ", end="")
     for name in args:
+        # using the provided variable number of arguments
         print(f"{name}, ", end="")
+
     print("nice to meet you!")
 
 
 try:
-    # won't work
+    # won't work, at least an argument is expected
     greeting()
 except TypeError as e:
     print("TypeError ...", e)
 
-# no arg
+# no extra argument
 greeting("Hello")
 
-# one arg
+# one extra argument
 greeting("Hello", "Tom")
 
-# two args
+# two extra arguments
 greeting("Hi", "Tom", "Bob")
 
-# three args
+# three extra arguments
 greeting("Hey", "Tom", "Bob", "Jim")
