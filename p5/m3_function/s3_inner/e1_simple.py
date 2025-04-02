44dfd0a13752240a2784de270dedefd753dbf32c
Emanuele <egalli64@gmail.com>
2025-04-02 11:36:21 +0200

    review

--- p5/m3_function/s3_inner/e1_simple.py
+++ p5/m3_function/s3_inner/e1_simple.py
@@ -10,13 +10,20 @@ Inner function - An example
 
 
 def outer(x):
+    """A function"""
+
     def inner(y):
+        """A function defined inside another function"""
         return y * 2
 
+    # invoke the inner function and return its result
     return inner(x)
 
 
 print("Calling outer():", outer(5))
 
 # the inner function can't be seen outside the function where it is defined
-# inner(5)
+try:
+    inner(5)
+except NameError as e:
+    print("Can't see an inner function outside its definition scope:", e)
