c0565ac26b27f76462da6abd8ae3657c2d771c96
Emanuele <egalli64@gmail.com>
2025-04-03 10:07:43 +0200

    more comments

--- p4/m2_serialization/s2_CSV/e1_writerow.py
+++ p4/m2_serialization/s2_CSV/e1_writerow.py
@@ -10,7 +10,8 @@ CSV - writerow
 
 import csv
 
-FILENAME = "friends.csv"
+PATH = "p4/m2_serialization/s2_CSV/"
+FILENAME = PATH + "friends.csv"
 HEADER = ["Name", "Age", "City"]
 
 friends = ["Tom", "Jenny", "Bob", "Kim", "Micky", "Lee"]
@@ -20,6 +21,8 @@ cities = ["Madrid", "Amsterdam", "Paris", "Stockholm", "Berlin", "London"]
 # write as CSV the values passed, row by row
 with open(FILENAME, "w", newline="") as file:
     writer = csv.writer(file)
+    # the header is seen just like any row
     writer.writerow(HEADER)
     for i in range(len(friends)):
+        # the row values should be passed in an iterable
         writer.writerow([friends[i], ages[i], cities[i]])
