fe1bec8e0769377b54dde746081a8a1632d8b744
Emanuele <egalli64@gmail.com>
2025-11-17 11:47:06 +0100

    clarify

--- p1/m3_number/s2_types/e2_int.py
+++ p1/m3_number/s2_types/e2_int.py
@@ -14,7 +14,10 @@ print("value is", value, type(value))
 
 print("value as boolean is", bool(value))
 
+# binary 10 is decimal 2
 print("the binary number 0b10 has decimal representation", 0b10)
+# octal 12_345_670 is decimal 2_739_128
 print("the octal number 0o12345670 has decimal representation", 0o12_345_670)
+# hexadecimal 0x1234567890ABCDEF is decimal 1_311_768_467_294_899_695
 print("the hexadecimal number 0x1234567890ABCDEF has decimal representation", end=" ")
 print(0x1234_5678_90AB_CDEF)
