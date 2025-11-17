5106d8a29f4990fe3f14a932d3f5c319d7b1167a
Emanuele <egalli64@gmail.com>
2025-11-17 11:46:45 +0100

    review

--- p1/m3_number/s2_types/e1_bool.py
+++ p1/m3_number/s2_types/e1_bool.py
@@ -17,5 +17,5 @@ print("now flag is", flag)
 print("True is 1, in numeric context:", 42 * True)
 print("False is 0:", 42 * False)
 
-print("Casted to boolean, each non-zero number is True:", bool(42) == True)
-print("Casted to boolean, zero is False:", bool(0.0) == False)
+print("Cast to boolean, any non-zero number is True:", bool(42) == True)
+print("Cast to boolean, zero is False:", bool(0.0) == False)
