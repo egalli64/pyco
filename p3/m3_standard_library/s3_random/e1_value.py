3dc4e51a2ecc591773fe6009b2e6530c7bcd9458
Emanuele <egalli64@gmail.com>
2025-11-18 12:53:42 +0100

    random value

---
+++ p3/m3_standard_library/s3_random/e1_value.py
@@ -0,0 +1,15 @@
+"""
+Python Course - Part 3
+
+https://github.com/egalli64/pyco
+
+Module 3 - Python Standard Library
+
+random: pick a value
+"""
+
+import random
+
+print("Random float value in [0, 1):", random.random())
+print("Random int value in [0, 9]:", random.randint(0, 9))
+print("Random float value in [0, 9]:", random.uniform(0, 9))
