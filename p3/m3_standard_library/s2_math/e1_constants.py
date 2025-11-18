39b6626d563b7ce37a4aa8274b61a6084129ef54
Emanuele <egalli64@gmail.com>
2025-11-18 10:59:51 +0100

    math module

---
+++ p3/m3_standard_library/s2_math/e1_constants.py
@@ -0,0 +1,19 @@
+"""
+Python Course - Part 3
+
+https://github.com/egalli64/pyco
+
+Module 3 - Python Standard Library
+
+Math: constants
+"""
+import math
+
+print("pi is", math.pi)
+print("e is", math.e)
+print("Positive infinity is", math.inf)
+print("Negative infinity is", -math.inf)
+print("Not a number is", math.nan)
+
+radius = 3
+print(f"A circle with radius {radius} has area {math.pi * radius ** 2}")
