7a4d452d79a160d2e2742919f700b77ba9c8dc5a
Emanuele <egalli64@gmail.com>
2025-11-20 08:37:18 +0100

    drop table

---
+++ p4/m5_oracle/e7_drop.py
@@ -0,0 +1,18 @@
+"""
+Python Course - Part 4
+
+https://github.com/egalli64/pyco
+
+Module 5 - Oracle DB
+"""
+
+import oracledb
+import db_config as db
+
+connection = oracledb.connect(user=db.usr, password=db.pwd, dsn=db.dsn)
+cursor = connection.cursor()
+
+cursor.execute("DROP TABLE test_py")
+
+cursor.close()
+connection.close()
