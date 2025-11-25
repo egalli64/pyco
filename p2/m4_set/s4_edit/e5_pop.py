3d740e96f6954fde09c48344cbbcfc1753c8d825
Emanuele <egalli64@gmail.com>
2025-11-25 11:16:55 +0100

    clarify

--- p2/m4_set/s4_edit/e5_pop.py
+++ p2/m4_set/s4_edit/e5_pop.py
@@ -8,11 +8,12 @@ Module 4 - Set
 Editing by pop()
 """
 
-# a set
-friends = {"bob", "kim", "tom"}
+# a set - no assumption on internal order
+friends = {"bob", "kim", "tom", "bill"}
 print("Friends:", friends)
 
 # pop() until the set is empty
+# if the set doesn't change, the iteration order is the same
 while friends:
     print(friends.pop(), "was a friend - popped!")
 
