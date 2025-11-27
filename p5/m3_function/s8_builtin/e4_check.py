69bf22e7c61566df17b5ea5122ae1ddc97f402dd
Emanuele <egalli64@gmail.com>
2025-11-27 13:17:17 +0100

    clarify

--- p5/m3_function/s8_builtin/e4_check.py
+++ p5/m3_function/s8_builtin/e4_check.py
@@ -10,11 +10,11 @@ Builtin - boolean checks on iterables (all - any)
 
 # all
 values = (42, 94, 12, 64, 87)
-print(f"Are all in {values} truthy?", all(values))
+print(f"Are all elements in {values} truthy?", all(values))
 
 values = ("Bob", "", "Tom")
-print(f"Are all in {values} truthy?", all(values))
+print(f"Are all elements in {values} truthy?", all(values))
 
 # any
-values = ("", None, {}, "0")
-print(f"Is there any truthy in {values}?", any(values))
+values = ("", None, {}, "0", 0)
+print(f"Is there any truthy element in {values}?", any(values))
